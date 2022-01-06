1a7731fad1a56123bacdf51eed9c0979f5dbb83e
Yuriy Podnebesov <dartwader032@gmail.com>
2022-01-06 14:00:40 +0200

    formatted Friends

--- src/components/WorkSpace/Friends/Friends.jsx
+++ src/components/WorkSpace/Friends/Friends.jsx
@@ -3,13 +3,10 @@ import style from './Friends.module.css';
 import FriendItem from './FriendItem/FriendItem';
 
 const Friends = (props) => {
-
-  let friendsElements = props.state.friends.map(f => <FriendItem id={f.id} name={f.name} surname={f.surname} />);
-  return (
-    <div className={style.content}>
-      {friendsElements}
-    </div>
-  )
-}
+  let friendsElements = props.state.friends.map((f) => (
+    <FriendItem id={f.id} name={f.name} surname={f.surname} />
+  ));
+  return <div className={style.content}>{friendsElements}</div>;
+};
 
 export default Friends;
