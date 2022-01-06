90337b70b02a8b6e4e1c7e4c8ed38443018ba5d6
Yuriy Podnebesov <dartwader032@gmail.com>
2022-01-06 13:59:30 +0200

    Profile styling

--- src/components/WorkSpace/Profile/Profile.jsx
+++ src/components/WorkSpace/Profile/Profile.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import Posts from '../Mainpage/Posts/Posts';
+import AddPost from './AddPost/AddPost';
 import style from './Profile.module.css';
 
-const Profile = (props) => {
-  
-    return <div className={style.content}>
-    <div>
-      <img src={props.profileData.imgAddress} />
+const Profile = ({ profileData, posts }) => {
+  return (
+    <div className={style.content}>
+      <div className={style.infoContainer}>
+        <div className={style.userAvatar}>
+          <img src={profileData.imgAddress} alt="avatar" />
+        </div>
+        <div>description</div>
+      </div>
+      <div className={style.postsHeading}>My Posts</div>
+      <AddPost />
+      <div>
+        <Posts posts={posts} />
+      </div>
     </div>
-    <div>
-      ava + description
-    </div>
-    <h2>My posts</h2>
-    <div>
-      <Posts posts={props.posts}/>
-    </div>
-  </div>
-}
+  );
+};
 
 export default Profile;
