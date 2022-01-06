f1bff4d8ea11c1373d2c056ce6737a56fed169ff
Yuriy Podnebesov <dartwader032@gmail.com>
2022-01-06 13:59:59 +0200

    MainPage styling

--- src/components/WorkSpace/Mainpage/Mainpage.jsx
+++ src/components/WorkSpace/Mainpage/Mainpage.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import style from './Mainpage.module.css';
 import Posts from './Posts/Posts';
-import AddPost from './AddPost/AddPost';
 
-function Mainpage(props) {
-    return (
-        <div>
-            <AddPost />
-            <h3>Posts</h3>
-            <Posts posts={props.state.posts} />
-        </div>
-    );
+function Mainpage({ posts }) {
+  return (
+    <div>
+      <div className={style.postsHeading}>Posts</div>
+      <Posts posts={posts} />
+    </div>
+  );
 }
 
 export default Mainpage;
