4b421181a6b268b21ea3825a6c5d8d4cd5f2f5fc
Yuriy Podnebesov <dartwader032@gmail.com>
2022-01-06 14:00:13 +0200

    Navbar styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import style from './Navbar.module.css';
-import { NavLink } from "react-router-dom";
-
+import { NavLink } from 'react-router-dom';
 
 const Navbar = (props) => {
-    return <nav className={style.nav}>
-        <div className={style.item}>
-            <NavLink to='/' activeClassName={style.activeLink}>Main page</NavLink>
-        </div>
-        <div className={style.item}>
-            <NavLink to='/profile' activeClassName={style.activeLink}>Profile</NavLink>
-        </div>
-        <div className={style.item}>
-            <NavLink to='/friends' activeClassName={style.activeLink}>Friends</NavLink>
-        </div>
-        <div className={style.item}>
-            <NavLink to='/chats' activeClassName={style.activeLink}>Chats</NavLink>
-        </div>
+  return (
+    <nav className={style.nav}>
+      <div className={style.item}>
+        <NavLink to="/" className={style.activeLink}>
+          Main page
+        </NavLink>
+      </div>
+      <div className={style.item}>
+        <NavLink to="/profile" className={style.activeLink}>
+          Profile
+        </NavLink>
+      </div>
+      <div className={style.item}>
+        <NavLink to="/friends" className={style.activeLink}>
+          Friends
+        </NavLink>
+      </div>
+      <div className={style.item}>
+        <NavLink to="/chats" className={style.activeLink}>
+          Chats
+        </NavLink>
+      </div>
     </nav>
-}
+  );
+};
 
 export default Navbar;
