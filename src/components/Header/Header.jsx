8201a074439961d271b3e75ea8c45a847344303f
Yuriy Podnebesov <dartwader032@gmail.com>
2022-01-09 00:36:46 +0200

    formatted Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import style from './Header.module.css';
 
 const Header = (props) => {
-    return <header className={style.header}>
-        <img src={props.logo} />
-        <div className={style.webName}>Nuf-Nuf Web</div>
+  return (
+    <header className={style.header}>
+      <img src={props.logo} alt="logo" />
+      <div className={style.webName}>Nuf-Nuf Web</div>
     </header>
-}
+  );
+};
 
 export default Header;
