f47a29023a3d024c81192d1519eff03ce33fda80
Mykola Kovalenko <kovnikur@gmail.com>
2022-01-12 19:55:27 +0200

    fixed viewing posts in Profile

--- src/components/WorkSpace/Mainpage/Posts/Posts.jsx
+++ src/components/WorkSpace/Mainpage/Posts/Posts.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import style from './Posts.module.css';
 import Post from './Post/Post';
+import { useSelector } from 'react-redux';
 
 const Posts = ({ posts }) => {
-  const postsElements = posts.map((p, i) => (
-    <Post
+  const user = useSelector((state) => state.user);
+  const friends = useSelector((state) => state.friends);
+  const postsElements = posts.map((p, i) => {
+    let userData = friends.find((f) => f.userId === p.userId);
+    if(userData !== undefined)
+      p.userName = userData.name + ' ' + userData.surname;
+    return <Post
       userName={p.userName}
       text={p.text}
       likesCount={p.likesCount}
       key={i}
     />
-  ));
+});
 
   return (
     <div className={style.postsBlock}>
